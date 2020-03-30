3be521864816e9dfe4fa2ca946cfaef2621b7ba8
Koki Shinjo <sktometometo@gmail.com>
2020-03-30 17:31:26 +0900

    fix for kinetic

--- src/test_node.cpp
+++ src/test_node.cpp
@@ -13,6 +13,10 @@ int main( int argc, char** argv )
     // tf2_ros::Buffer tf_buffer(ros::Duration(max_duration));
     // tf2_ros::TransformListener tf_listener(tf_buffer);
 
+    // case 0: can not be compiled.
+    tf2_ros::Buffer tf_buffer(ros::Duration(max_duration));
+    tf2_ros::TransformListener tf_listener(static_cast<tf2_ros::Buffer>(tf_buffer));
+
     // case 1: can not be compiled.
     // tf2_ros::Buffer tf_buffer(ros::Duration(max_duration));
     // tf2_ros::TransformListener tf_listener(tf_buffer,nh,true);
@@ -23,8 +27,8 @@ int main( int argc, char** argv )
     // tf2_ros::TransformListener tf_listener(tf_buffer);
 
     // case 3: can be compiled.
-    tf2_ros::Buffer tf_buffer(ros::Duration(10.0));
-    tf2_ros::TransformListener tf_listener(tf_buffer);
+    // tf2_ros::Buffer tf_buffer(ros::Duration(10.0));
+    // tf2_ros::TransformListener tf_listener(tf_buffer);
 
     // case 4: can be compiled.
     // tf2_ros::Buffer tf_buffer(ros::Duration(max_duration),true);
